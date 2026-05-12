29f0a89331e09b5707ae17b43831d7fe91e16dfb
Xuting Liu <xutingl@seas.upenn.edu>
2026-05-12 19:56:20 -0400

    chore: bootstrap project (pyproject, venv, smoke test)

---
+++ tests/test_smoke.py
@@ -0,0 +1,4 @@
+import twisted_analysis
+
+def test_package_imports():
+    assert twisted_analysis.__version__ == "0.1.0"
---
+++ twisted_analysis/__init__.py
@@ -0,0 +1,2 @@
+"""TwistedAnalysis: AllToAll gap analysis on twisted-torus topologies."""
+__version__ = "0.1.0"

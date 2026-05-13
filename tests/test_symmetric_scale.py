d074ab5a52a3095acdc7b0ba94aef37365e8f784
Xuting Liu <xutingl@seas.upenn.edu>
2026-05-13 00:22:24 -0400

    test(lp): scale test for symmetric ILP on 4x8 (slow)

---
+++ tests/test_symmetric_scale.py
@@ -0,0 +1,15 @@
+import pytest
+
+from twisted_analysis.topology import Topology, ILPRouter
+from twisted_analysis.model import AllToAll
+from twisted_analysis.lp.symmetric import solve_symmetric_makespan
+
+
+@pytest.mark.slow
+def test_symmetric_4x8_tractable():
+    """4x8 symmetric scheduling ILP should solve in under 5 minutes."""
+    t = Topology(slice=(4, 8))
+    r = ILPRouter(t)
+    w = AllToAll(t, r, msg_size=1)
+    m, _ = solve_symmetric_makespan(t, r, list(w.flows), T_upper=w.lower_bound * 4)
+    assert m >= w.lower_bound
